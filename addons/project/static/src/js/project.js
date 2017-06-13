1d04ccf5aa1a0d5c4850c419a29ae6a46679f31d
Vandan Shah <vas@odoo.com>
2017-06-13 10:58:54 +0530

    [IMP] project: improvments in set cover image usability

--- addons/project/static/src/js/project.js
+++ addons/project/static/src/js/project.js
@@ -51,7 +51,7 @@ KanbanRecord.include({
         }
 
         function open_cover_images_dialog(attachment_ids) {
-            var coverID = self.record.displayed_image_id && self.record.displayed_image_id.raw_value[0];
+            var coverID = self.record.displayed_image_id.raw_value;
             var $content = $(QWeb.render("project.SetCoverModal", {
                 cover_id: coverID,
                 attachment_ids: attachment_ids,
@@ -59,15 +59,46 @@ KanbanRecord.include({
             var $imgs = $content.find('img');
             var dialog = new Dialog(self, {
                 title: _t("Set a Cover Image"),
-                buttons: [{text: _t("Select"), classes: 'btn-primary', close: true, disabled: !coverID, click: function () {
+                buttons: [{text: _t("Select"), classes: (attachment_ids.length) ? 'btn-primary' : 'o_hidden', close: true, disabled: !coverID, click: function () {
                     var $img = $imgs.filter('.o_selected');
                     var data = {
                         id: $img.data('id'),
                         display_name: $img.data('name')
                     };
                     self._updateRecord({displayed_image_id: data});
-                }}, {text: _t("Remove Cover Image"), close: true, click: function () {
+                }}, {text: _t("Remove Cover Image"), classes:(coverID) ? '' : 'o_hidden', close: true, click: function () {
                     self._updateRecord({displayed_image_id: false});
+                }}, {text: _t("Upload and Set"), classes: (attachment_ids.length) ? '' : 'btn-primary o_attach', close: true, click: function(){
+                    var $upload_input = $('<input type="file" accept="image/*" name="files[]"/>');
+                    $upload_input.click();
+                    $upload_input.on('change', function (e) {
+                        var file = e.target.files[0];
+                        var filereader = new FileReader();
+                        filereader.readAsDataURL(file);
+                        filereader.onloadend = function (upload) {
+                            var file_data = upload.target.result;
+                            file_data = file_data.split(',')[1];
+                            var data = {
+                                res_model: self.modelName,
+                                res_id: self.id,
+                                datas: file_data,
+                                name: file.name,
+                                datas_fname: file.name,
+                                mimetype: file.type
+                            };
+                            self._rpc({
+                                model: 'ir.attachment',
+                                method: 'create',
+                                args: [data],
+                            }).then(function(e){
+                                var data = {
+                                    id: e,
+                                    display_name: file.name
+                                };
+                                self._updateRecord({displayed_image_id: data});
+                            });
+                        };
+                    });
                 }}, {text: _t("Discard"), close: true}],
                 $content: $content,
             }).open();
