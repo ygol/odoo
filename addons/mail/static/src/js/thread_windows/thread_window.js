18d3c4e2a5d0df94514d49afc21ed5fccecfd50b
svs-odoo <svs@odoo.com>
2018-10-31 11:04:37 +0100

    [WIP] mail: promisify /thread_windows

--- addons/mail/static/src/js/thread_windows/thread_window.js
+++ addons/mail/static/src/js/thread_windows/thread_window.js
@@ -77,7 +77,7 @@ var ThreadWindow = AbstractThreadWindow.extend({
             });
         }
 
-        return $.when(superDef, composerDef);
+        return Promise.all([superDef, composerDef]);
     },
 
     //--------------------------------------------------------------------------
@@ -234,7 +234,7 @@ var ThreadWindow = AbstractThreadWindow.extend({
             .autocomplete({
                 source: function (request, response) {
                     self.call('mail_service', 'searchPartner', request.term, 10)
-                        .done(response);
+                        .then(response);
                 },
                 select: function (event, ui) {
                     // remember partner ID so that we can replace this window
