f82795c451fbf4536e8a92a82c3f615869a122ec
Pierre Paridans <app@odoo.com>
2019-07-16 12:29:20 +0200

    [IMP] point_of_sale: add test for UsernameWidget

--- addons/point_of_sale/static/tests/pos_tests.js
+++ addons/point_of_sale/static/tests/pos_tests.js
@@ -1,7 +1,9 @@
 odoo.define("point_of_sale.PointOfSaleTests", function(require) {
     "use strict";
 
+    const RamStorage = require("web.RamStorage");
     const { createActionManager } = require("web.test_utils");
+    const PosDB = require("point_of_sale.DB");
 
     async function createPointOfSale(params) {
         const actionManager = await createActionManager(params);
@@ -12,6 +14,14 @@ odoo.define("point_of_sale.PointOfSaleTests", function(require) {
     async function isPointOfSaleLoaded() {
         // FIXME: this is hacky and shouldn't be exposed that way...
         return window.posmodel.chrome.ready;
+    }
+
+    async function loadPointOfSale(params) {
+        const actionManager = await createPointOfSale(params);
+        // FIXME: this is hacky and shouldn't be exposed that way...
+        window.posmodel.db = new PosDB({ storage: new RamStorage() });
+        await isPointOfSaleLoaded();
+        return actionManager;
     }
 
     QUnit.module("Point of Sale");
@@ -209,6 +219,27 @@ odoo.define("point_of_sale.PointOfSaleTests", function(require) {
                 );
 
                 pos.destroy();
+            });
+
+            QUnit.module("UsernameWidget", function() {
+                QUnit.test("basic rendering", async function(assert) {
+                    assert.expect(1);
+
+                    const pos = await loadPointOfSale({
+                        data: this.data,
+                        session: this.session,
+                    });
+
+                    assert.strictEqual(
+                        pos
+                            .$(".pos-topheader .username")
+                            .text()
+                            .trim(),
+                        "Mitchell Admin"
+                    );
+
+                    pos.destroy();
+                });
             });
         }
     );
