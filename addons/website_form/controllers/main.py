1ef580eed1f0e617f4b71a02bae56132c7f13353
Kaushalya Mandaliya <kma@odoo.com>
2019-09-03 15:45:48 +0530

    [IMP] website_form: known partners  add as followers if in a CC
    PURPOSE
    
    People sending tickets through the website might add their vendors or consultant
    in the CC field but the vendor or consultant is not added as a follower of the
    task.
    
    SPECIFICATIONS
    
    It should be added automatically so they can easily follow what's happening to
    their clients. Hence, this commit adds known partners as followers if added in
    a CC.
    
    LINKS
    
    PR https://github.com/odoo/odoo/pull/48497
    Task 1974030

--- addons/website_form/controllers/main.py
+++ addons/website_form/controllers/main.py
@@ -199,6 +199,13 @@ class WebsiteForm(http.Controller):
             values.update({'reply_to': values.get('email_from')})
         record = request.env[model_name].with_user(SUPERUSER_ID).with_context(mail_create_nosubscribe=True).create(values)
 
+        # email_cc add as a follower if they are known partner
+        if values.get('email_cc') and issubclass(request.registry[model_name], request.registry['mail.thread.cc']):
+            emails = record._mail_cc_sanitized_raw_dict(values.get('email_cc')).values()
+            partner_ids = request.env['res.partner'].sudo().search([('email', 'in', list(emails))]).ids
+            if partner_ids:
+                record.message_subscribe(partner_ids=partner_ids)
+
         if custom or meta:
             _custom_label = "%s\n___________\n\n" % _("Other Information:")  # Title for custom fields
             if model_name == 'mail.mail':
