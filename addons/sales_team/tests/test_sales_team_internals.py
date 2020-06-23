2f8851dbebbd31ce9bf624f3b8bd0a6e9557661d
Thibault Delavallée <tde@odoo.com>
2020-06-23 11:56:21 +0200

    fixup st test

--- addons/sales_team/tests/test_sales_team_internals.py
+++ addons/sales_team/tests/test_sales_team_internals.py
@@ -4,24 +4,44 @@
 import psycopg2
 
 from odoo import exceptions
+from odoo.addons.mail.tests.common import mail_new_test_user
 from odoo.addons.sales_team.tests.common import TestSalesMC
-from odoo.tests.common import users
+from odoo.tests.common import users, TransactionCase
+from odoo.tools import mute_logger
 
 
+class TestCornerCases(TransactionCase):
 
-class TestCornerCases(TestSalesMC):
-
-    @users('user_sales_manager')
     def test_unicity(self):
-        """ Even archived memberships raise the unicity constraint """
-        membership = self.sales_team_1_m1.with_user(self.env.user)
-        membership.write({'active': False})
-        membership.flush()
+        """ Even archived memberships raise the unicity constraint.
 
-        with self.assertRaises(psycopg2.IntegrityError):
+        Note: redoing the data set to avoid clashing with SavepointCase as
+        we expect a db-level assert """
+        user_sales_leads = mail_new_test_user(
+            self.env, login='user_sales_leads',
+            name='Laetitia Sales Leads', email='crm_leads@test.example.com',
+            company_id=self.env.user.company_id.id,
+            notification_type='inbox',
+            groups='sales_team.group_sale_salesman_all_leads,base.group_partner_manager',
+        )
+        sales_team_1 = self.env['crm.team'].create({
+            'name': 'Test Sales Team',
+            'sequence': 5,
+            'company_id': False,
+            'user_id': self.env.user.id,
+        })
+        sales_team_1_m1 = self.env['crm.team.member'].create({
+            'user_id': user_sales_leads.id,
+            'crm_team_id': sales_team_1.id,
+        })
+
+        sales_team_1_m1.write({'active': False})
+        sales_team_1_m1.flush()
+
+        with self.assertRaises(psycopg2.IntegrityError), mute_logger('odoo.sql_db'):
             self.env['crm.team.member'].create({
-                'user_id': self.user_sales_leads.id,
-                'crm_team_id': self.sales_team_1.id,
+                'user_id': user_sales_leads.id,
+                'crm_team_id': sales_team_1.id,
             })
 
 
