cae4b1bb387365eb9df7bb82145e4b047b1cc341
Thibault Francois <tfr@openerp.com>
2020-06-23 15:02:00 +0200

    [FIX] odoo_referral: move gift icon to usermenu
    Move the gift icon to reach the referral page
    to the user menu.
    
    Remove unnecessary code since we cannot display
    the count in the user menu

--- addons/odoo_referral/models/res_users.py
+++ addons/odoo_referral/models/res_users.py
@@ -20,25 +20,6 @@ _logger = logging.getLogger(__name__)
 class Users(models.Model):
     _inherit = 'res.users'
 
-    @api.model
-    def get_referral_updates_count_for_current_user(self):
-        if tools.config.options['test_enable']:
-            return 0
-        token = self.env.user._get_or_generate_referral_token()
-        if not token:
-            return 0
-
-        try:
-            response = requests.get(DEST_SERVER_URL + '/referral/notifications/' + token, timeout=10)
-        except Exception as e:
-            if not getattr(threading.currentThread(), 'testing', False):
-                _logger.exception("Failed to fetch referral gift notifications")
-            return 0
-        if not response.ok:
-            return 0
-
-        return response.json().get('updates_count', 0)
-
     def _get_or_generate_referral_token(self):
         self.ensure_one()
         db_secret = self.env['ir.config_parameter'].sudo().get_param('database.secret').encode('utf-8')
