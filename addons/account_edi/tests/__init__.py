3e401014871ed0e5a2dfede050f0c9468aeb2957
Pierre Paridans <app@odoo.com>
2020-08-25 14:41:29 +0000

    [IMP] website_event_track_online: allow customization of the PWA's name
    This commit adds a website-specific and customizable app name for the
    Events' Progressive Web Application, configurable through the website's
    settings.
    
    Defaults to '<website_name> Events'.
    
    X-original-commit: 423076ddf1da0fc61d9a18dc19bbbbf900e3dad6

--- addons/account_edi/tests/__init__.py
+++ addons/account_edi/tests/__init__.py
@@ -2,4 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import common
+<<<<<<< HEAD:addons/account_edi/tests/__init__.py
 from . import test_edi
+=======
+from . import test_website
+>>>>>>> 6a7a67b1206... temp:addons/website_event_track_online/tests/__init__.py
