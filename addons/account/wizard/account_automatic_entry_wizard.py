6d6d0796b1dc69e59dbbc31200ae76cb02edf4a9
wan <wan@odoo.com>
2020-08-25 11:53:34 +0200

    [FIX] account: active ids on cutoff
    In some cases, the active ids were not correctly set (id of the
    account.move)
    
    (clean a forgotten print too)

--- addons/account/wizard/account_automatic_entry_wizard.py
+++ addons/account/wizard/account_automatic_entry_wizard.py
@@ -45,7 +45,6 @@ class AutomaticEntryWizard(models.TransientModel):
             if not (0.0 < record.percentage <= 100.0):
                 raise UserError(_("Percentage must be between 0 and 100"))
             if record.percentage != 100 and record.action != 'change_period':
-                print(record.percentage)
                 raise UserError(_("Percentage can only be set for Change Period method"))
 
     @api.depends('percentage', 'move_line_ids')
